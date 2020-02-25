c7d0dea08e3d4bcb2b4c63b8d2bc34db542a0dd0
Ichlas Wardy Gustama <cahyo4713@gmail.com>
2020-02-25 15:16:46 +0700

    Fix & Update Patch
    Fixed IF ELSE CASE QUERY
    Updated New Key 'genre' and 'country' in JSON Result

--- post.php
+++ post.php
@@ -6,7 +6,7 @@ use simple_curl\curl;
 $query = base64_decode($_GET['query']);
 $server = $_GET['server'];
 
-if (empty($query) && !isset($query))
+if (empty($query) || !isset($query))
 {
     die();
 } else {
